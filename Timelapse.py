6659fb8871969c41ae2f987d5f4f57cc4145aadf
isabelnic <hannahisabelnicholson@gmail.com>
2024-04-21 12:54:21 +0100

    modifying folders

--- Timelapse.py
+++ Timelapse.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
 
-root_folder = 'C:\\Users\\hanna\\OneDrive - University of Edinburgh\\Desktop\\Photography-code\\Photography\\to-process\\'
-
+save_folder = 'to-process\\'
+photo_folder = 'C:\\Users\\hanna\\OneDrive - University of Edinburgh\\Pictures\\2024\\plant-evolution\\'
+fps = 35
 
 
 def get_image_dirs():
     # Get the directories of the images
     image_dirs = []
-    for root, dirs, files in os.walk(root_folder):
+    for root, dirs, files in os.walk(photo_folder):
         for f in files:
             if (f.endswith('.JPG') or f.endswith('.jpg')):
                 image_dirs.append(os.path.join(root, f))
@@ -22,8 +23,8 @@ def get_image_dirs():
 
 def stitch_videos():
     images = get_image_dirs()
-    writer = imageio.get_writer(root_folder + f'timepalse_{datetime.now().strftime("%Y%m%d_%H%M%S")}.mp4', 
-                                fps=20)
+    writer = imageio.get_writer(save_folder + f'timepalse_{datetime.now().strftime("%Y%m%d_%H%M%S")}.mp4', 
+                                fps=fps)
     for i in tqdm(range(len(images))):
         ## Load the image
         image = imageio.v2.imread(images[i])
